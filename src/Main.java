42a0bfed3f8eb5492625283a1514b960dbd9d2a1
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-03-04 04:57:46 +0300

    homework redone

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Gryffindor[] gryf = new Gryffindor[5];
+        Gryffindor[] gryf = new Gryffindor[5];                                      // Четыре факультета Хогвардца
         Puffenduy[] puff = new Puffenduy[5];
         Kogtevran[] kogt = new Kogtevran[5];
         Slizerin[] sliz = new Slizerin[5];
         Hogwarts[] students = {
-            new Hogwarts("Гарри Потер", 80, 90),
-            new Hogwarts("Гермиона Грейнджер", 90, 70),
-            new Hogwarts("Рон Уизли", 70, 70),
-            new Hogwarts("Захария Смит", 80, 80),
-            new Hogwarts("Седрик Диггори", 90, 80),
-            new Hogwarts("Джастин Финч-Флетчли", 70, 80),
-            new Hogwarts("Чжоу Чанг", 80, 90),
-            new Hogwarts("Падма Патил", 70, 80),
-            new Hogwarts("Маркус Белби", 80, 80),
-            new Hogwarts("Драко Малфой", 80, 80),
-            new Hogwarts("Грэхэм Монтегю", 60, 80),
-            new Hogwarts("Грегори Гойл", 70, 60),
-        };
+            new Hogwarts("Гарри Потер", 50, 20),
+            new Hogwarts("Гермиона Грейнджер", 60, 10),
+            new Hogwarts("Рон Уизли", 40, 10),
+            new Hogwarts("Захария Смит", 40, 0),
+            new Hogwarts("Седрик Диггори", 60, 10),
+            new Hogwarts("Джастин Финч-Флетчли", 50, 20),
+            new Hogwarts("Чжоу Чанг", 40, 10),
+            new Hogwarts("Падма Патил", 50, 0),
+            new Hogwarts("Маркус Белби", 60, 10),
+            new Hogwarts("Драко Малфой", 50, 20),
+            new Hogwarts("Грэхэм Монтегю", 40, 0),
+            new Hogwarts("Грегори Гойл", 40, 10),
+        };                                                                              //  Ученики прибывают в Хогвартс
         System.out.println("Школа магии и волшебства ХОГВАРДС :");
         for (int i = 0; i < students.length; i++) {
             System.out.println(students[i]);
@@ -28,10 +28,10 @@ public class Main {
                 Hogwarts.compareBetween (students[i], students[j]);
             }
         }
-        System.out.println();
+        System.out.println();                                           //  Шляпа-распределительница зачисляет учеников в факультеты
         System.out.println("факультет Гриффиндор :");
         for (int i = 0; i < 3; i++) {
-            gryf[i] = choiceDepartment(students[i]);          // зачисление Гарри, Гермионы и Рона на факультет Гриффиндор
+            gryf[i] = choiceDepartmentGryffindor(students[i]);          // зачисление Гарри, Гермионы и Рона на факультет Гриффиндор
             System.out.println(gryf[i]);
         }
         System.out.println();
@@ -41,7 +41,7 @@ public class Main {
         System.out.println();
         System.out.println("факультет Пуффендуй :");
         for (int i = 3; i < 6; i++) {
-            puff[i-3] = choiceDepartment(students[i], 0);          // зачисление на факультет Пуффендуй
+            puff[i-3] = choiceDepartmentPuffenduy(students[i]);          // зачисление на факультет Пуффендуй
             System.out.println(puff[i-3]);
         }
         System.out.println();
@@ -51,7 +51,7 @@ public class Main {
         System.out.println();
         System.out.println("факультет Когтевран :");
         for (int i = 6; i < 9; i++) {
-            kogt[i-6] = choiceDepartment(students[i], 0, 0);          // зачисление на факультет Когтевран
+            kogt[i-6] = choiceDepartmentKogtevran(students[i]);          // зачисление на факультет Когтевран
             System.out.println(kogt[i-6]);
         }
         System.out.println();
@@ -61,7 +61,7 @@ public class Main {
         System.out.println();
         System.out.println("факультет Слизерин :");
         for (int i = 9; i < 12; i++) {
-            sliz[i-9] = choiceDepartment(students[i], 0, 0, 0);     // зачисление на факультет Слизерин
+            sliz[i-9] = choiceDepartmentSlizerin(students[i]);          // зачисление на факультет Слизерин
             System.out.println(sliz[i-9]);
         }
         System.out.println();
@@ -76,19 +76,19 @@ public class Main {
         Hogwarts.compareBetween (puff[1], sliz[1]);
         Hogwarts.compareBetween (kogt[1], sliz[1]);
     }
-    public static Gryffindor choiceDepartment(Hogwarts student) {
+    public static Gryffindor choiceDepartmentGryffindor(Hogwarts student) {
             return new Gryffindor( student.getNAME(), student.getPowerOfMagic(), student.getDistanceOfTransgression(),
                     (int) (70 + 31*Math.random()), (int) (70 + 31*Math.random()) , (int) (70 + 31*Math.random()));
     }
-    public static Puffenduy choiceDepartment(Hogwarts student, int a) {
+    public static Puffenduy choiceDepartmentPuffenduy(Hogwarts student) {
         return new Puffenduy( student.getNAME(), student.getPowerOfMagic(), student.getDistanceOfTransgression(),
                 (int) (70 + 31*Math.random()), (int) (70 + 31*Math.random()) , (int) (70 + 31*Math.random()));
     }
-    public static Kogtevran choiceDepartment(Hogwarts student, int a, int b) {
+    public static Kogtevran choiceDepartmentKogtevran(Hogwarts student) {
         return new Kogtevran( student.getNAME(), student.getPowerOfMagic(), student.getDistanceOfTransgression(),
                 (int) (70 + 31*Math.random()), (int) (70 + 31*Math.random()) , (int) (70 + 31*Math.random()), (int) (70 + 31*Math.random()));
     }
-    public static Slizerin choiceDepartment(Hogwarts student, int a, int b, int c) {
+    public static Slizerin choiceDepartmentSlizerin(Hogwarts student) {
         return new Slizerin( student.getNAME(), student.getPowerOfMagic(), student.getDistanceOfTransgression(),
                 (int) (70 + 31*Math.random()), (int) (70 + 31*Math.random()) , (int) (70 + 31*Math.random()),
                 (int) (70 + 31*Math.random()), (int) (70 + 31*Math.random()));
